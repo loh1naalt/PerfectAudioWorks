d5f8cb877b4fcd79995842a5ee477c9222c76c17
Vladislav Kolodiy <vladislavkolodiy45@gmail.com>
2025-09-10 13:11:12 +0300

    urgent about window fix...

--- src/PAW_GUI/about_paw_gui.cpp
+++ src/PAW_GUI/about_paw_gui.cpp
@@ -1,15 +1,19 @@
-#include "about_paw_gui.h" 
-#include "ui_about_paw_gui.h" 
+#include "about_paw_gui.h"
+#include "ui_about_paw_gui.h"
+#include <QLabel>
 
-
-About_PAW_gui::About_PAW_gui(QWidget *parent) 
+About_PAW_gui::About_PAW_gui(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::About_PAW_gui) 
+    , ui(new Ui::About_PAW_gui)
 {
     ui->setupUi(this);
+
+
+    // QLabel* extraLabel = new QLabel("Extra dynamic info", this);
+    // ui->dynamicLayout->addWidget(extraLabel);
 }
 
-About_PAW_gui::~About_PAW_gui() 
+About_PAW_gui::~About_PAW_gui()
 {
     delete ui;
 }
